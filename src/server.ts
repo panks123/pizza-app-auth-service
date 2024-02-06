226e88112fee0ccb9c6a44daf3ac2a9d6d39fb03
Pankaj <pankajkumarhzb787@gmail.com>
2024-02-07 00:00:27 +0530

    express app setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
 import { Config } from "./config";
+import app from "./app";
 
-console.log("PORT", Config.PORT);
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log("Listening on port", PORT);
+    });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+startServer();
