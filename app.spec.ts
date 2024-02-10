faf73f3484cacf72adb06af9ba20abc4c8b64a4c
Pankaj <pankajkumarhzb787@gmail.com>
2024-02-10 16:56:43 +0530

    add test setup

---
+++ app.spec.ts
@@ -0,0 +1,15 @@
+import app from "./src/app";
+import { calculateDiscount } from "./src/utils";
+import request from "supertest";
+
+describe("App", () => {
+  it("should calculate the discount", () => {
+    const result = calculateDiscount(100, 5);
+    expect(result).toBe(5);
+  });
+
+  it("should return 200 status", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+  });
+});
