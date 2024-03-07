588cd6c49cdbd232b5abc5cd893cdde920b4ebb9
Pankaj <pankajkumarhzb787@gmail.com>
2024-03-08 00:08:00 +0530

    refactor tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,8 +5,6 @@ import createHttpError from "http-errors";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
 import { JwtPayload } from "jsonwebtoken";
-import { AppDataSource } from "../config/data-source";
-import { RefreshToken } from "../entity/RefreshToken";
 import { TokenService } from "../services/TokenService";
 
 export class AuthController {
@@ -49,22 +47,8 @@ export class AuthController {
 
         const accessToken = this.tokenService.generateAccessToken(payload);
 
-        // Persist the refresh token in DB
-        const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
-        const refreshTokenRepository =
-          AppDataSource.getRepository(RefreshToken);
-        const newRefreshToken = await refreshTokenRepository.save({
-          user: user,
-          expiresAt: new Date(Date.now() + MS_IN_YEAR),
-        });
-
-        // const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
-        //   algorithm: "HS256",
-        //   expiresIn: "1y",
-        //   issuer: "auth-service",
-        //   jwtid: String(newRefreshToken.id) // id of the saved referesh token in DB
-        // });
-
+        const newRefreshToken =
+          await this.tokenService.persistRefreshToken(user);
         const refreshToken = this.tokenService.generateRefreshToken({
           ...payload,
           id: newRefreshToken.id,
--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -3,9 +3,14 @@ import path from "path";
 import { JwtPayload, sign } from "jsonwebtoken";
 import createHttpError from "http-errors";
 import { Config } from "../config";
+import { User } from "../entity/User";
+import { RefreshToken } from "../entity/RefreshToken";
+import { Repository } from "typeorm";
 // import { TokenPayload } from '../types';
 
 export class TokenService {
+  constructor(private refreshTokenRepository: Repository<RefreshToken>) {}
+
   generateAccessToken(payload: JwtPayload) {
     let privateKey: Buffer;
     try {
@@ -31,6 +36,16 @@ export class TokenService {
       issuer: "auth-service",
       jwtid: String(payload.id), // id of the saved referesh token in DB
     });
+    return refreshToken;
+  }
+
+  async persistRefreshToken(user: User) {
+    const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
+    const refreshToken = await this.refreshTokenRepository.save({
+      user: user,
+      expiresAt: new Date(Date.now() + MS_IN_YEAR),
+    });
+
     return refreshToken;
   }
 }
