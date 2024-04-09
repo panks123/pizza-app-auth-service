991a4d3ba28b05c8f815feb2e959250e8aa34282
Pankaj <pankajkumarhzb787@gmail.com>
2024-04-10 00:09:04 +0530

    fix typescript build

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
   // synchronize: Config.NODE_ENV === "test" || Config.NODE_ENV === "dev",
   synchronize: false,
   logging: false,
-  entities: ["src/entity/*.ts"],
+  entities: ["src/entity/*.{ts,js}"],
   migrations: ["src/migration/*.ts"],
   subscribers: [],
 });
